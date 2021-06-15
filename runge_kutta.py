8399110e0e8a5efc5ec8548efedfaa4f6deb15b5
Vignesh Desmond <vignesh.desmond@gmail.com>
2021-06-15 23:17:01 +0530

    attribute change

--- runge_kutta.py
+++ runge_kutta.py
@@ -5,9 +5,9 @@ from attractors import Attractors
 
 class RK(Attractors):
 
-    def __init__(self , initial_r, attractor, **kwargs):
+    def __init__(self , initial_coord, attractor, **kwargs):
         super(RK, self).__init__(attractor, **kwargs)
-        self.r = initial_r
+        self.coord = initial_coord
         self.X = []
         self.Y = []
         self.Z = []
@@ -18,12 +18,12 @@ class RK(Attractors):
         attractor_func = getattr(RK, self.attractor)
         
         for _ in time_scale:
-            self.X.append(self.r[0])
-            self.Y.append(self.r[1])
-            self.Z.append(self.r[2])
+            self.X.append(self.coord[0])
+            self.Y.append(self.coord[1])
+            self.Z.append(self.coord[2])
 
-            k1 = h*attractor_func(self, self.r)
-            self.r += k1
+            k1 = h*attractor_func(self, self.coord)
+            self.coord += k1
     
     def RK2(self, a, b, N, method='heun'):
         h = (b-a)/N
@@ -31,39 +31,39 @@ class RK(Attractors):
         attractor_func = getattr(RK, self.attractor)
 
         def heun():
-            rt = self.r
-            k1 = h*attractor_func(self, self.r)
+            rt = self.coord
+            k1 = h*attractor_func(self, self.coord)
 
-            self.r = self.r+ k1
-            k2 = h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord+ k1
+            k2 = h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r += (k1+k2)/2
+            self.coord += (k1+k2)/2
         
         def imp_poly():
-            rt = self.r
-            k1 = h*attractor_func(self, self.r)
+            rt = self.coord
+            k1 = h*attractor_func(self, self.coord)
 
-            self.r = self.r+ k1/2
-            k2 = h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord+ k1/2
+            k2 = h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r += k2
+            self.coord += k2
 
         def ralston():
-            rt = self.r
-            k1 = h*attractor_func(self, self.r)
+            rt = self.coord
+            k1 = h*attractor_func(self, self.coord)
 
-            self.r = self.r+ 3*k1/4
-            k2 = h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord+ 3*k1/4
+            k2 = h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r += (k1+2*k2)/3
+            self.coord += (k1+2*k2)/3
 
         for _ in time_scale:
-            self.X.append(self.r[0])
-            self.Y.append(self.r[1])
-            self.Z.append(self.r[2])
+            self.X.append(self.coord[0])
+            self.Y.append(self.coord[1])
+            self.Z.append(self.coord[2])
             eval(method)()
 
     def RK3(self, a, b, N):
@@ -72,22 +72,22 @@ class RK(Attractors):
         attractor_func = getattr(RK, self.attractor)
     
         for _ in time_scale:
-            self.X.append(self.r[0])
-            self.Y.append(self.r[1])
-            self.Z.append(self.r[2])
+            self.X.append(self.coord[0])
+            self.Y.append(self.coord[1])
+            self.Z.append(self.coord[2])
             
-            rt = self.r
-            k1 = h*attractor_func(self, self.r)
+            rt = self.coord
+            k1 = h*attractor_func(self, self.coord)
 
-            self.r = self.r + k1/2
-            k2 = h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord + k1/2
+            k2 = h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r = self.r - k1 + 2*k2
-            k3 = h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord - k1 + 2*k2
+            k3 = h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r += (k1+4*k2+k3)/6
+            self.coord += (k1+4*k2+k3)/6
     
     def RK4(self, a, b, N):
         h = (b-a)/N
@@ -95,26 +95,26 @@ class RK(Attractors):
         attractor_func = getattr(RK, self.attractor)
 
         for _ in time_scale:
-            self.X.append(self.r[0])
-            self.Y.append(self.r[1])
-            self.Z.append(self.r[2])
+            self.X.append(self.coord[0])
+            self.Y.append(self.coord[1])
+            self.Z.append(self.coord[2])
             
-            rt = self.r
-            k1 = h*attractor_func(self, self.r)
+            rt = self.coord
+            k1 = h*attractor_func(self, self.coord)
             
-            self.r = self.r+ k1/2
-            k2 = h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord+ k1/2
+            k2 = h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r = self.r + k2/2
-            k3 = h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord + k2/2
+            k3 = h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r = self.r+k3
-            k4=h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord+k3
+            k4=h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r += (k1+2*k2+2*k3+k4)/6
+            self.coord += (k1+2*k2+2*k3+k4)/6
     
     def RK5(self, a, b, N):
         h = (b-a)/N
@@ -122,31 +122,31 @@ class RK(Attractors):
         attractor_func = getattr(RK, self.attractor)
 
         for _ in time_scale:
-            self.X.append(self.r[0])
-            self.Y.append(self.r[1])
-            self.Z.append(self.r[2])
+            self.X.append(self.coord[0])
+            self.Y.append(self.coord[1])
+            self.Z.append(self.coord[2])
             
-            rt = self.r
-            k1 = h*attractor_func(self, self.r)
+            rt = self.coord
+            k1 = h*attractor_func(self, self.coord)
             
-            self.r = self.r+ k1/4
-            k2 = h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord+ k1/4
+            k2 = h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r = self.r + k2/8 + k1/8
-            k3 = h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord + k2/8 + k1/8
+            k3 = h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r = self.r+k3 - k2/2 + k3
-            k4=h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord+k3 - k2/2 + k3
+            k4=h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r = self.r - 3*k1/16 + 9*k4/16
-            k5=h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord - 3*k1/16 + 9*k4/16
+            k5=h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r = self.r - 3*k1/7 + 2*k2/7 + 12*k3/7 - 12*k4/7 + 8*k5/7
-            k6=h*attractor_func(self, self.r)
-            self.r = rt
+            self.coord = self.coord - 3*k1/7 + 2*k2/7 + 12*k3/7 - 12*k4/7 + 8*k5/7
+            k6=h*attractor_func(self, self.coord)
+            self.coord = rt
 
-            self.r += (7*k1+32*k3+12*k4+32*k5+7*k6)/90
+            self.coord += (7*k1+32*k3+12*k4+32*k5+7*k6)/90
