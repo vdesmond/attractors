d46b8aa052b6313ca8725e43e0de6afd3dae004a
Vignesh Desmond <vignesh.desmond@gmail.com>
2021-06-24 18:13:16 +0530

    gitignore

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click', 'Matplotlib',
+        'Matplotlib',
     ],
     entry_points={
         'console_scripts': [
